d6dfb56051c5ce8f072010939cf9f1f10ad50623
Chris <88049272+V0idMatr1x@users.noreply.github.com>
2022-12-02 14:47:26 -0600

    add: showcase

--- src/lib/Showcase.svelte
+++ src/lib/Showcase.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
-
+    
 </script>
 
 <main>
     <div class="showcase-container">
+        <div class="showcase-intro">
+            <h2>I am...</h2>
+            <p>A Software Engineering Professional & Hacker of Things..</p>
+        </div>
 
+        <div class="showcase-content">
+            
+        </div>
     </div>
 </main>
