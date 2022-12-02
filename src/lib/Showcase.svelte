2c76e6aa07a63d31abf2ab1d9e3afd4911a4f03d
Chris <88049272+V0idMatr1x@users.noreply.github.com>
2022-12-02 15:36:07 -0600

    add: showcase effect

--- src/lib/Showcase.svelte
+++ src/lib/Showcase.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
+    import Typewriter from 'svelte-typewriter';
+    
+    const introHeader: string = "I am...";
+    const introText: string = "A Software Engineering Professional & Hacker of Things..";
+
     
 </script>
 
 <main>
     <div class="showcase-container">
         <div class="showcase-intro">
-            <h2>I am...</h2>
-            <p>A Software Engineering Professional & Hacker of Things..</p>
-        </div>
-
-        <div class="showcase-content">
-            
+            <h2>{introHeader}</h2>
+            <p><Typewriter>{introText}</Typewriter></p>
         </div>
     </div>
 </main>
