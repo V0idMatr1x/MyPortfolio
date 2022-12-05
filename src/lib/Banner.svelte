8925660d53495cbbf8bd3db97131ff94f2e28e79
Chris <88049272+V0idMatr1x@users.noreply.github.com>
2022-12-04 18:58:37 -0600

    add: update banner

--- src/lib/Banner.svelte
+++ src/lib/Banner.svelte
@@ -2,14 +2,14 @@
     <div class="skill-banner__content">
         <ul>
             <li id="skill">Enterprise Development</li>
-            <li id="skill">Full Stack</li>
-            <li id="skill">Research</li>
-            <li id="skill">Linux</li>
             <li id="skill">Software Architecture</li>
-            <li id="skill">DevOps (CI/CD)</li>
-            <li id="skill">Software Optimization</li>
             <li id="skill">Systems Programming</li>
+            <li id="skill">Software Optimization</li>
+            <li id="skill">Full Stack</li>
+            <li id="skill">Linux</li>
             <li id="skill">Cybersecurity</li>
+            <li id="skill">DevOps (CI/CD)</li>
+            <li id="skill">Research</li>
             <li id="skill">Problem Solving</li>
             <li id="skill">Agile</li>
         </ul>
