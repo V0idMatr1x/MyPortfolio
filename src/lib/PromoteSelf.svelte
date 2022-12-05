29685784753a25606b875f923ea22d4a9f5677dd
Chris <88049272+V0idMatr1x@users.noreply.github.com>
2022-12-04 22:51:56 -0600

    update: links

--- src/lib/PromoteSelf.svelte
+++ src/lib/PromoteSelf.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     const callToAction: string = "🚀 Hire me for a contract?";
+    const emailLink: string = "mailto:matr1xware@v0idmatr1x.com";
 </script>
 
 <div class="protion-container">
-    <a id="call-to__action" href="mailto:matr1xware@v0idmatr1x.com">{callToAction}</a>
+    <a id="call-to__action" href={emailLink}>{callToAction}</a>
 </div>
